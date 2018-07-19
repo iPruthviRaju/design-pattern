e79697401addb99c49a7411703d3f682f5e820f1
Pruthvi Raju <pruthvi.raju@live.com>
2018-07-19 20:10:05 +0530

    Singleton initial approach, initial program

---
+++ 1-creational/5-singleton/0001_First_Singleton/0001_First_Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001_First_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.Instance;
+            fromEmployee.PrintDetails("Hi I'm employee..");
+
+            Singleton fromAdmin = Singleton.Instance;
+            fromAdmin.PrintDetails("Hi I'm admin..");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1-creational/5-singleton/0001_First_Singleton/0001_First_Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001_First_Singleton
+{
+    public class Singleton
+    {
+        private static int _counter = 0;
+
+        private static Singleton _instance = null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Singleton();
+
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            _counter++;
+            Console.WriteLine("I'm the counter from default constructor, count - {0}", _counter);
+        }
+
+        public void PrintDetails(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}

53a6aae21cb085997be0b0de64911b76bef63784
Pruthvi Raju <pruthvi.raju@live.com>
2018-07-20 23:30:51 +0530

    Fix for parallel call with new object and lock check on it

--- 1-creational/5-singleton/0001_First_Singleton/0001_First_Singleton/Singleton.cs
+++ 1-creational/5-singleton/0001_First_Singleton/0001_First_Singleton/Singleton.cs
@@ -12,13 +12,21 @@ namespace _0001_First_Singleton
 
         private static Singleton _instance = null;
 
+        private static readonly object obj = new object();
+
+
         public static Singleton Instance
         {
             get
             {
                 if (_instance == null)
-                    _instance = new Singleton();
-
+                {
+                    lock (obj)
+                    {
+                        if (_instance == null)
+                            _instance = new Singleton();
+                    }
+                }
                 return _instance;
             }
         }
